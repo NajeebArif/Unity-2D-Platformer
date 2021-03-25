a258b49d6cd5fda18b61fa69279ecf4bf4ec5c8d
Najeeb Arif <NajeebArif786@outlook.com>
2021-03-25 21:43:42 +0530

    adding platform

--- Assets/Scripts/PlatformPlayer.cs
+++ Assets/Scripts/PlatformPlayer.cs
@@ -10,6 +10,7 @@ public class PlatformPlayer : MonoBehaviour
 
     private Rigidbody2D _body;
     private Animator _anim;
+    private BoxCollider2D _box;
 
 
     // Start is called before the first frame update
@@ -17,6 +18,7 @@ public class PlatformPlayer : MonoBehaviour
     {
         _body = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -25,7 +27,17 @@ public class PlatformPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
-        if (Input.GetKey(KeyCode.Space))
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, max.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+        bool grounded = false;
+        if (hit != null) {
+            grounded = true;
+        }
+        _body.gravityScale = grounded && deltaX == 0 ? 0 : 1;
+        if (grounded && Input.GetKey(KeyCode.Space))
             _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         _anim.SetFloat("speed", Mathf.Abs(deltaX));
         if (!Mathf.Approximately(deltaX, 0)) {
